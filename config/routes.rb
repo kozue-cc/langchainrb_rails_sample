56d3ead2ecc932eff19a0e99a4a0cfe69c2f471e
KozueTabuchi <kozue.tabuchi@treetops-code-crafters.com>
2023-11-03 19:54:21 +0900

    とりあえずrecipesコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/update'
+  get 'recipes/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
