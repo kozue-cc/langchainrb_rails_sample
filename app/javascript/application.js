982f5adc55ae91c1c377e0eee782538f41e6011b
KozueTabuchi <kozue.tabuchi@treetops-code-crafters.com>
2023-11-04 17:25:57 +0900

    デザインの修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,6 +7,3 @@ import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
-
-// Your own JavaScript code
-// ...
